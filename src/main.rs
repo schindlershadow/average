985aa0531804bda590a0a124b00d50f03952af84
schindlershadow <schindlershadow@gmail.com>
2017-10-20 18:13:45 -0400

    added mean function

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,33 @@ fn main() {
     //list of integers
     let mut integers = vec![1, 3, 2, 4, 5, 6, 7, 8, 9, 10];
 
+    println!("Input Vector");
+    print_vec(&integers);
+
     //sort the vector
     integers.sort();
 
+    println!("Sorted Vector");
     print_vec(&integers);
+
+    let mean = vec_mean(&integers);
+    println!("\nmean: {}", mean);
+
 }
 
-fn print_vec(vec: &Vec<u32>) {
+fn print_vec(vec: &Vec<i32>) {
     for element in vec.iter() {
-        println!("{}", element);
+        print!("{} ", element);
     }
+    println!();
+}
+
+fn vec_mean(vec: &Vec<i32>) -> f32 {
+    let mut total = 0.0;
+    for element in vec.iter() {
+        let float = *element as f32;
+        total = total + float;
+    }
+
+    total / vec.len() as f32
 }
